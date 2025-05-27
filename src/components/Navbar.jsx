3150d74f9f7131de90f08680855f29011f815536
user1 <user01@em/com>
2025-05-27 13:53:11 +0300

    update UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='p-3 bg-blue-900 text-white text-center'>
-      Welcome to my wiki
+    <div className=''>
+      <div className='p-3 bg-blue-900 text-white flex justify-around items-center'>
+        <img className='w-10  rounded-xl' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdwOtTuhnUF9d3veTXIqjKa77kTiFY5dWMiA&s" alt="" />
+        <span className='text-center text-xl font-bold'>Welcome to my wiki</span>
+      </div>
     </div>
   )
 }
