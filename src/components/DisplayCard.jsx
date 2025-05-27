eaf1354cd0a767990cf54aca6fe65246336b6e48
user1 <user01@em/com>
2025-05-27 13:15:56 +0300

    update ui

--- src/components/DisplayCard.jsx
+++ src/components/DisplayCard.jsx
@@ -7,8 +7,6 @@ function DisplayCard() {
 
   const { id } = useParams();
 
-  console.log(id);
-
   useEffect(() => {
     console.log("hello");
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -25,6 +23,7 @@ function DisplayCard() {
       <div className="p-3 lg:w-8/10">
         <div className="flex flex-col items-center gap-5 py-5 bg-white rounded-xl p-3">
           <h1><span className="font-bold text-2xl">{item.title}</span></h1>
+          <img className="rounded-xl w-5/10" src="https://images.pexels.com/photos/31913883/pexels-photo-31913883/free-photo-of-scenic-mountain-landscape-with-grazing-horses.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
           <h1>{item.body}</h1>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,23 +16,18 @@ function Home() {
       })
   }, [])
 
-
-// {
-//     "userId": 1,
-//     "id": 1,
-//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-//     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-// }
-
   return (
     <div className="bg-blue-100 flex justify-center">
       <div className="lg:w-9/10">
         <ul className="p-5 flex flex-wrap justify-center  gap-3">
           {item.map((element, index) => (
             <li key={index}>
-                <div className=" flex flex-col justify-between h-[30vh]  w-[35vh] p-3 rounded bg-white  hover:translate-1 shadow-2xl">
+                <div className=" flex flex-col justify-between h-[50vh]  w-[35vh] p-3 rounded bg-white  hover:translate-1 shadow-2xl">
                     <h1><span className="font-bold">{element.title}</span></h1>
-                    <Link className="rounded p-1 text-white bg-blue-500 hover:bg-blue-900 text-center" to={`${element.id}`}>Read more</Link>
+                    <div className="flex flex-col gap-5">
+                      <img className="rounded-xl" src="https://images.pexels.com/photos/31913883/pexels-photo-31913883/free-photo-of-scenic-mountain-landscape-with-grazing-horses.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                      <Link className="rounded p-1 text-white bg-blue-500 hover:bg-blue-900 text-center" to={`${element.id}`}>Read more</Link>
+                    </div>
                 </div>
             </li>
           ))}
