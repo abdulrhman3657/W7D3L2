35479e2b56c3674ff0400507acc8a11e7004175a
user1 <user01@em/com>
2025-05-27 13:36:17 +0300

    update UI

--- src/components/DisplayCard.jsx
+++ src/components/DisplayCard.jsx
@@ -18,8 +18,8 @@ function DisplayCard() {
   }, [id]);
 
   return (
-    <div className="flex flex-col items-center gap-3  h-screen bg-blue-100 shadow-2xl">
-      <Link to={"../"} className="my-5 rounded p-1 text-white bg-blue-500 hover:bg-blue-900 text-center">Go Back</Link>
+    <div className="flex flex-col items-center gap-3  h-screen bg-linear-to-r from-gray-300 to-blue-300 shadow-2xl">
+      <Link to={"../"} className="my-5 rounded p-1 w-30 lg:text-xl text-white bg-blue-500 hover:bg-blue-900 text-center">Go Back</Link>
       <div className="p-3 lg:w-8/10">
         <div className="flex flex-col items-center gap-5 py-5 bg-white rounded-xl p-3">
           <h1><span className="font-bold text-2xl">{item.title}</span></h1>
